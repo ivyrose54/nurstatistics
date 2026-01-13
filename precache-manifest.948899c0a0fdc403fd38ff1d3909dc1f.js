ca856ad3afc47d33bdefded8cac660615047741e
ivyrose54 <bual.ivyrose@gmail.com>
2026-01-13 21:33:47 +0800

    Updates

---
+++ precache-manifest.948899c0a0fdc403fd38ff1d3909dc1f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9419ed53527e26db678088626fdd5d",
+    "url": "/nurstatistics/index.html"
+  },
+  {
+    "revision": "eee241e651269b0ce570",
+    "url": "/nurstatistics/static/css/main.02ce2062.chunk.css"
+  },
+  {
+    "revision": "d81e6221b5661b559045",
+    "url": "/nurstatistics/static/js/2.07a9d5ce.chunk.js"
+  },
+  {
+    "revision": "4059c5910822ea23b8a08f3c5b8f5905",
+    "url": "/nurstatistics/static/js/2.07a9d5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee241e651269b0ce570",
+    "url": "/nurstatistics/static/js/main.f63d5849.chunk.js"
+  },
+  {
+    "revision": "0a03355e70e7753b6ebc",
+    "url": "/nurstatistics/static/js/runtime-main.6fdb1a93.js"
+  },
+  {
+    "revision": "11e96296a383c15aca883a601cb806a7",
+    "url": "/nurstatistics/static/media/bglanding.11e96296.png"
+  },
+  {
+    "revision": "93b4c1ae66d7e79fb18675016b51c776",
+    "url": "/nurstatistics/static/media/cardiology.93b4c1ae.jpg"
+  },
+  {
+    "revision": "2741fc1cf5c220a6302b338ce71353b4",
+    "url": "/nurstatistics/static/media/dashboard.2741fc1c.png"
+  },
+  {
+    "revision": "3bfb647fce622920ee3918a71d9401e0",
+    "url": "/nurstatistics/static/media/departments.3bfb647f.png"
+  },
+  {
+    "revision": "5548b8814b1257618c4a56ce3403c27a",
+    "url": "/nurstatistics/static/media/diagnosis.5548b881.png"
+  },
+  {
+    "revision": "247026997abc94f15e8cdcbcfbd72143",
+    "url": "/nurstatistics/static/media/footer-image.24702699.png"
+  },
+  {
+    "revision": "952375c1dd5d44b4d41d6fae33bd7659",
+    "url": "/nurstatistics/static/media/geriatrics.952375c1.jpg"
+  },
+  {
+    "revision": "90127d8e7b1719ee0e0adf2e4cd6e51d",
+    "url": "/nurstatistics/static/media/helpdesk.90127d8e.png"
+  },
+  {
+    "revision": "4bb128ed844c66e36b4518cf3468be2c",
+    "url": "/nurstatistics/static/media/logo.4bb128ed.png"
+  },
+  {
+    "revision": "992cda2f8bc9f84ff5a226550b216ff8",
+    "url": "/nurstatistics/static/media/logout.992cda2f.png"
+  },
+  {
+    "revision": "db74d2632d013e9edf7ac1903cc22674",
+    "url": "/nurstatistics/static/media/medical.db74d263.png"
+  },
+  {
+    "revision": "039496f0a32b12586a896b38ac2ff36f",
+    "url": "/nurstatistics/static/media/nurstatistics.039496f0.png"
+  },
+  {
+    "revision": "5a0274e984c2ed0ddd7d0159b5ad7a2c",
+    "url": "/nurstatistics/static/media/opthalmology.5a0274e9.jpg"
+  },
+  {
+    "revision": "ef4c65f85503de4ced60c691c7980b69",
+    "url": "/nurstatistics/static/media/personal.ef4c65f8.png"
+  },
+  {
+    "revision": "9d2ef95da6314143a8f6f75b90eff857",
+    "url": "/nurstatistics/static/media/symptoms.9d2ef95d.png"
+  }
+]);
